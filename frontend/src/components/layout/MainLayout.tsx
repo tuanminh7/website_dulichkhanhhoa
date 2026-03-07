4e1c5f92fdd5b057df04540f9b83002249d52f65
NTH-Nguyen <nthn300607@gmail.com>
2026-03-07 19:38:51 +0700

    update

--- frontend/src/components/layout/MainLayout.tsx
+++ frontend/src/components/layout/MainLayout.tsx
@@ -7,7 +7,7 @@ const MainLayout: React.FC = () => {
     return (
         <div className="min-h-screen flex flex-col bg-gray-50">
             <Navbar />
-            <main className="flex-grow">
+            <main className="grow">
                 <Outlet />
             </main>
             <Footer />
